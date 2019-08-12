5dbc8375785383d2596a8d395e80d5ec9d496dd0
caitao <951832669@qq.com>
2019-08-12 23:09:54 +0800

    添加依赖

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,75 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <draggable
+      class="box"
+      v-model="myArray"
+      :group="{ name: 'people', pull: 'clone', put: false }"
+    >
+      <div
+        class="item"
+        v-for="element in myArray"
+        :key="element.id"
+      >{{element.name}}</div>
+    </draggable>
+    <div style="height: 50px"></div>
+    <draggable
+      class="box"
+      v-model="myArray2"
+      group="people"
+      @change="change"
+    >
+      <div
+        class="item"
+        v-for="(item,index) in myArray2"
+        @click="itemClick(index)"
+        :key="index"
+      >{{item.name}}</div>
+    </draggable>
   </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
+import VInput from './input'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  components: { draggable },
+  data () {
+    return {
+      myArray: [
+        { id: 1, name: '1' },
+        { id: 2, name: '2' },
+        { id: 3, name: '3' },
+        { name: '4', key: 'fore' },
+        VInput.data()
+      ],
+      myArray2: [
+        { id: 4, name: '4' },
+        { id: 5, name: '5' },
+        { id: 6, name: '6' }
+      ]
+    }
+  },
+  methods: {
+    change () {
+      console.log(this.myArray2)
+      console.log(VInput.data())
+    },
+    itemClick (index) {
+      console.log(index)
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style scoped lang="less">
+.box{
+  padding: 10px;
+  border: 1px solid red;
+  display: flex;
+  .item{
+    border: 1px solid black;
+    width: 50%;
+  }
 }
 </style>
---
+++ src/components/input.vue
@@ -0,0 +1,18 @@
+<template>
+  <input type="text">
+</template>
+<script>
+export default {
+  name: 'v-input',
+  data () {
+    return {
+      type: 'input',
+      value: '',
+      name: 'haha'
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
